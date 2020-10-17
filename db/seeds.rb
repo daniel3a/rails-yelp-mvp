fcba30067cbda24e787e9abab714bad8f9d6f440
daniel assis <daniel.augusto.assis@gmail.com>
2020-10-17 12:39:35 -0300

    Yelp app rake ok without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "starting adding restaurants"
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "restaurant: #{restaurant.id} is created"
+end
+
+puts "done!"
